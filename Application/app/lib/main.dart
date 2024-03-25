c01885d1ff50bd220367ebfcd7a9c2c31fdbd124
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-03-25 10:26:04 +0530

    exercise submitted

---
+++ Application/app/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My First App"),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Image.asset("assets/mix-happy-woman-in-a-jump.png"),
+      ),
+    );
+  }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Flutter Exercise (Flutter Mapp YouTube)
+# task:adding icon to app, adding image from icon8.com and making change in App Bar
